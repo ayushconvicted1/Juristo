a13b7911f5f936876d1e9372554772b2fde21a65
Anshuman Tiwari <tanshuman145@gmail.com>
2025-01-07 15:02:32 +0530

    new updates added

--- app/api/legaldocs/generate/route.js
+++ app/api/legaldocs/generate/route.js
@@ -19,7 +19,7 @@ async function connectToDatabase() {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      serverSelectionTimeoutMS: 50000, // Timeout after 5s instead of 30s
     });
     console.log("Connected to MongoDB");
   } catch (error) {
