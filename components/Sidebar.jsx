bfbbc2b7af98f9c21668af16ca8cb1a326cbb4cc
Anshuman Tiwari <tanshuman145@gmail.com>
2025-01-18 23:12:23 +0530

    logo updated

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -92,10 +92,13 @@ export default function Sidebar() {
   };
 
   return (
-    <div className="flex h-full w-[280px] flex-col border-r bg-background py-4">
+    <div className="flex h-full w-[280px] flex-col border-r  py-4">
       <div className="flex items-center gap-2 px-4 pb-4">
-        <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-foreground text-background font-bold">
-          J
+        <div className="flex h-8 w-8 items-center justify-center rounded-lg font-bold">
+          <img
+            src="https://res.cloudinary.com/dc9msi1wn/image/upload/v1737221626/LOGO_1_nj85xe.png"
+            alt="juristo"
+          />
         </div>
         <button
           className="text-xl font-medium tracking-tight bg-transparent shadow-none"
